bab86e3cf2321ff2c3aed87b371781dbb0687e7c
RioMulya <riomulya75@gmail.com>
2023-01-19 16:46:55 +0700

    router setup

---
+++ src/router.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceive from './pages/requests/RequestsReceive.vue';
+import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    {
+      path: '/coaches',
+      component: CoachesList,
+      children: [
+        {
+          path: '/:id',
+          component: CoachDetail,
+          children: [{ path: '/contact', component: ContactCoach }],
+        },
+      ],
+    },
+    { path: '/register', component: CoachRegistration },
+    { path: '/requests', component: RequestsReceive },
+    { path: '/:cathAll(.*)', component: NotFound },
+  ],
+});
+
+export default router;
